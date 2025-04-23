6ba0a3e0d226335053d5b4a7e716f30a3e8d51fb
nichnikov <a.nichnikov@gmail.com>
2025-04-23 12:40:39 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AITK-673-query-classificator
 Сделать классификатор на основе разметки
+
+0 - Калькулятор
+1 - НПД
+2 - Образцы
+3 - Рекомендации
+
+
+https://sbert.net/docs/cross_encoder/pretrained_models.html
+
+главный скрипт здесь:
+https://sbert.net/docs/package_reference/sentence_transformer/losses.html#denoisingautoencoderloss
+
+можно будет попробовать еще (если тема получит развитие):
+https://huggingface.co/deepvk/USER-bge-m3 (русский язык)
---
+++ st_classifier_train.py
@@ -0,0 +1,41 @@
+"""
+https://sbert.net/docs/package_reference/sentence_transformer/losses.html#denoisingautoencoderloss
+"""
+
+import os
+import pandas as pd
+from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, SentenceTransformerTrainingArguments, losses
+from datasets import Dataset
+
+data_df = pd.read_csv(os.path.join("data", "labled_data.csv"), sep="\t")
+print(data_df)
+
+sentences = data_df["query"].to_list()
+labels = data_df["label"].to_list()
+
+model = SentenceTransformer("paraphrase-multilingual-mpnet-base-v2")
+
+# E.g. 0: sports, 1: economy, 2: politics
+train_dataset = Dataset.from_dict({
+    "sentence":sentences,
+    "label": labels,
+})
+
+loss = losses.BatchSemiHardTripletLoss(model)
+args = SentenceTransformerTrainingArguments(num_train_epochs=1)
+
+trainer = SentenceTransformerTrainer(
+    args=args,
+    model=model,
+    train_dataset=train_dataset,
+    loss=loss,
+)
+
+trainer.train()
+
+final_output_dir = "query_classifier"
+# trainer.save_model("query_classifier")
+model.save_pretrained(final_output_dir)
+
+text = "Как рассчитать налоговый вычет при покупке квартиры?"
+trainer.predict(text)
