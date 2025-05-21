70787255b808255133bc31baca8de10fc0bf1323
nichnikov <a.nichnikov@gmail.com>
2025-05-21 16:49:14 +0300

    ref

--- st_classifier_train.py
+++ st_classifier_train.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, SentenceTransformerTrainingArguments, losses
 from datasets import Dataset
 
-data_df = pd.read_csv(os.path.join("data", "labled_data.csv"), sep="\t")
+data_df = pd.read_csv(os.path.join("data", "labled_data_large.csv"), sep="\t")
 print(data_df)
 
 sentences = data_df["query"].to_list()
@@ -22,7 +22,8 @@ train_dataset = Dataset.from_dict({
 })
 
 loss = losses.BatchSemiHardTripletLoss(model)
-args = SentenceTransformerTrainingArguments(num_train_epochs=1)
+args = SentenceTransformerTrainingArguments(num_train_epochs=1, 
+                                            output_dir="training_output_new")
 
 trainer = SentenceTransformerTrainer(
     args=args,
@@ -33,8 +34,8 @@ trainer = SentenceTransformerTrainer(
 
 trainer.train()
 
-final_output_dir = "query_classifier"
-# trainer.save_model("query_classifier")
+final_output_dir = "query_classifier_new"
+trainer.save_model(final_output_dir)
 model.save_pretrained(final_output_dir)
 
 text = "Как рассчитать налоговый вычет при покупке квартиры?"
