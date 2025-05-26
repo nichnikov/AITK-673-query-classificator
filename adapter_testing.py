5b27195ffe1ab7c48c3b3d59059c61f1096982de
nichnikov <a.nichnikov@gmail.com>
2025-05-26 16:53:53 +0300

    ref

--- adapter_testing.py
+++ adapter_testing.py
@@ -1,17 +1,32 @@
+import os
+import pandas as pd
 from transformers import TextClassificationPipeline
-from transformers import XLMRobertaConfig
+
 from adapters import AutoAdapterModel
+
 from transformers import AutoTokenizer
-import numpy as np
-from transformers import AutoModel, TrainingArguments, EvalPrediction
-from adapters import AdapterTrainer
 
+test_df = pd.read_csv(os.path.join("data", "labled_data_for_testing.csv"), sep="\t")
+print(test_df)
 
+test_dicts = test_df.to_dict(orient="records")
 tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-m3')
 model = AutoAdapterModel.from_pretrained("./train_model")
 model.train_adapter("queries_4classes") # Магическая активация адаптера
 
-classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores=True)
+classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, top_k=1)
 
-class_res = classifier("Оформление и расчет отпускных в 2025 году")
-print(class_res)
+texts = [d["query"] for d in test_dicts[:500]]
+print(classifier(texts))
+
+test_results = []
+for num, d in enumerate(test_dicts):
+    class_res = classifier(d["query"])
+    res_d = class_res[0][0]
+    print(num, "query:", d["query"], "label:", res_d["label"], "score:", res_d["score"])
+    test_results.append({**d, **res_d})
+
+test_results_df = pd.DataFrame(test_results)
+print(test_results_df)
+
+test_results_df.to_csv(os.path.join("data", "labled_data_with_test_results.csv"), sep="\t")
